5eb41c49b4ce22997f7b27c314bf92adcc6397f0
Marnel Fresh'eur <gmlginolias@gmail.com>
2021-04-13 01:55:16 +0100

    Doing exercices using useState and useEffet

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# react-app
-The react modern build setup
+## React-App
+I could use **parcel** instead of **create-react-app** but parcel was occurred a lot of errors. There are also the
+fact that **react-app** also have the **fast refresh** nowadays so all was set to make me use react-app instead of parcel.
+
+## Hooks
+In react, hooks are functions allowing use to create our components using function instead of classes in order to be 
+able to separate ou logic, treatments from our rendering. There are:
+
+### useState
+**useState** allow us to set a state to our component create using function. It takes the initial value and return an array 
+of two elements:
+- **the state variable**: it can be named as we want and contains the value of the state
+- **the state setter**: a function that can be used to change the value of the state.
+
+**E.g.**:\
+`const [state, setState] = useState(initialValue)`\
+`const handler = () => setState(s => s+1)`\
+\
+**useState** can be used as much as we need states in our component.
+
+### useEffet
+It's used to define a function that can be called when a given list of variables' value change. It takes two arguments:
+- **a callback**: the function that will be executed
+- **an array**: list of variables that should be observed. 
+
+**useEffet** executes the callback when the `componentDidMount` even when the second parameter is an empty array.\
+The callback should return a function that will be executed when the `componentDidUnmount`\
+
+**useEffet** can be used as much as we need states in our component.
+
+## Create a hook
+It's possible to create our own hooks using the predefined ones. 
+
--- src/App/App.js
+++ src/App/App.js
@@ -1,38 +1,131 @@
-import React, {useState, forwardRef, Component, createRef} from 'react'
-import './App.css'
+import React, {useState, useEffect} from 'react'
 
-function useIncrement(init, step) {
-  const [state, setState] = useState(init)
-  const increment = function () {
-    setState(state+step)
+function useIncrement(init = 0, step = 1) {
+  const [count, setCount] = useState(init)
+
+  const increment = (e) => {
+    e.preventDefault()
+    setCount(c => c + step)
   }
-  return [state, increment]
+  return [count, increment]
 }
 
-const Incrementer = forwardRef(function (props, ref) {
-  const [count, increment] = useIncrement(props.initial, props.step)
-  const [count2, increment2] = useIncrement(props.initial, 1)
+function useFeth(url) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true)
+  useEffect(function () {
+    (async function () {
+      const response = await fetch(url)
+      const responseData = await response.json()
+      if(response.ok) {
+        setData(responseData)
+      }else{
+        setData(false)
+      }
+      setLoading(false)
+    })()
+  }, [])
 
-  return <>
-    <button ref={ref} onClick={increment}>
-      {props.children} {count}
-    </button>
-    <button onClick={increment2}>Default step {count2}</button>
-  </>
-})
+  return [loading, data]
+}
 
-export default class App extends Component{
+function PostTable_old() {
+  const [events, setEvents] = useState([])
+  const [loading, setLoading] = useState(true)
+  useEffect(function () {
+    (async function () {
+      const response = await fetch('https://localhost:8000/timeline')
+      const data = await response.json()
+      if (response.ok) {
+        setEvents(data)
+      } else {
+        alert(JSON.stringify(response))
+      }
+      setLoading(false)
+    })()
+  }, [])
 
-  constructor(props) {
-    super(props)
-    this.inc = createRef()
+
+  if(loading) {
+    return <p>Chargement...</p>
+  }
+  const list = events.map((event) => <tr key={event.id}>
+    <td>{event.title}</td>
+    <td>{event.subtitle}</td>
+  </tr>)
+
+  return <table>
+    {list}
+  </table>
+}
+
+function PostTable() {
+  let [loading, data] = useFeth('https://localhost:8000/timeline')
+  while(loading) {
+    return <p>Chargement...</p>
   }
 
-  render() {
-    return <div>
-      Hello World
-      <Incrementer initial={0} step={5} ref={this.inc}>Marnel</Incrementer>
-    </div>
-  }
+  return <table>
+    {data.map((event) => <tr key={event.id}>
+      <td key={'td1' + event.id}>{event.title}</td>
+      <td key={'td2' + event.id}>{event.subtitle}</td>
+    </tr>)}
+  </table>
+}
 
+
+function useAutoIncrement(init = 0, step = 1) {
+  const [count, setCount] = useState(init)
+
+  useEffect(function () {
+    const time = setTimeout(function () {
+      setCount(c => c + step)
+    }, 1000)
+    return function () {
+      clearTimeout(time)
+    }
+  }, [count])
+
+  return count
+}
+
+function useToggle(initial = false) {
+  const [state, setState] = useState(initial)
+
+  const handleToggle = function () {
+    setState((s) => !s)
+  }
+  return [state, handleToggle]
+}
+
+function Compteur ({initial, step}) {
+  const [count, increment] = useIncrement(initial, step)
+
+  return <button onClick={increment}>
+    Increment {count}
+  </button>
+}
+
+function Compteur2 ({initial, step}) {
+  const count = useAutoIncrement(initial, step)
+
+  return <button>
+    Increment {count}
+  </button>
+}
+
+
+export function App() {
+  const [compteurVisible, toggleCompteur] = useToggle(true)
+
+  return <div>
+    <label htmlFor="toggler">
+      Afficher le compteur
+      <input id="toggler" type="checkbox" onChange={toggleCompteur} checked={compteurVisible} />
+    </label>
+    <br/>
+    {compteurVisible && <Compteur2 initial={0} step={2} />}
+
+    <PostTable/>
+  </div>
 }
